6157fa95b5ebe9d770488d92d90e7fb7003de358
MH221B <22127131@student.hcmus.edu.vn>
2024-02-28 20:19:47 +0700

    turn BPB into class

---
+++ BPB.h
@@ -0,0 +1,22 @@
+#include "read.h"
+#include "entry.h"
+
+class BPB
+{
+private:
+    vector<string> storedValues;
+public:
+    BPB(const char *diskPath) {readSector(diskPath, 0, 512, storedValues);}
+    int findNTFSIdentifier();
+    int findBeginningOfBPB();
+    int getSectorSize();
+    int getSc();
+    int getst();
+    int getHeadNumber();
+    int getBeginningSectorOfLogicalPartition();
+    int getSv();
+    int startingClusterOfMFT();
+    int getSizeOfAMFTEntry();
+    void printBPBInfo();
+    void printBPB();
+};
