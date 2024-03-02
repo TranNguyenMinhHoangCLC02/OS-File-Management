f84337a8414c66c3edbec2b8f300bb5a9e2fa924
MH221B <22127131@student.hcmus.edu.vn>
2024-03-02 14:47:18 +0700

    read full MFT

--- main.cpp
+++ main.cpp
@@ -40,7 +40,7 @@ int main()
     // Entries entry;
     // entry.input(entries);
     // entry.print();
-    LPCWSTR disk = L"\\\\.\\E:";
+    LPCWSTR disk = L"\\\\.\\C:";
     BPB bpb(disk);
     // bpb.printBPBInfo();
     // bpb.printBPB();
@@ -48,9 +48,61 @@ int main()
     int Sc = bpb.getSc();
     vector<vector<string>> entries;
     // read_MFT(MFTStart, Sc, disk, entries);
-    // vector<string> MFT;
-    read_MFT2(MFTStart, Sc, entries);
-    printMFT(entries);
+    vector<string> MFT;
+    // read_MFT2(MFTStart, Sc, entries);
+    readSector(L"\\\\.\\C:", MFTStart * Sc, 1024, MFT);
+    for (int i = 0; i < MFT.size(); i++)
+    {
+        if (i % 16 == 0)
+            cout << "\n";
+        cout << MFT[i] << " ";
+    }
+    cout << "\n";
+    int MFTSize = 0;
+    vector<string> temp = MFT;
+    int offsetToAttribute = convertHexadecimalToDecimal(convertStringToLittleEdian(getStringFromVector(temp, 0x14, 0x15 - 0x14 + 1)));
+    temp.erase(temp.begin(), temp.begin() + offsetToAttribute);
+    for (int i = 0; i < temp.size(); i++)
+    {
+        if (i % 16 == 0)
+            cout << "\n";
+        cout << temp[i] << " ";
+    }
+    cout << "\n";
+    while (temp.empty() == false)
+    {
+        vector<string> temp2;
+        for (int i = 0; i < 16; i++)
+        {
+            temp2.push_back(temp[i]);
+        }
+        NTFSAttributeHeader header(temp2);
+        if (MFTSize == MFTSize + header.getAttributeSize())
+            break;
+        MFTSize += header.getAttributeSize();
+        cout << "current size: " << MFTSize << "\n";
+        temp.erase(temp.begin(), temp.begin() + header.getAttributeSize());
+    }
+    cout << "MFT Size: " << MFTSize << endl;
+    MFTStart++;
+    for (unsigned long long i = MFTStart * Sc; i < MFTStart * Sc + MFTSize; i += 1)
+    {
+        vector<string> temp;
+        readSector(L"\\\\.\\C:", i, 1024, temp);
+        entries.push_back(temp);
+    }
+    for (int i = 0; i < entries.size(); i++)
+    {
+        for (int j = 0; j < entries[i].size(); j++)
+        {
+            if (j % 16 == 0)
+                cout << "\n";
+            cout << entries[i][j] << " ";
+        }
+        cout << "\n";
+        cout << "\n";
+    }
+    // printMFT(entries);
     system("pause");
     return 0;
 }
